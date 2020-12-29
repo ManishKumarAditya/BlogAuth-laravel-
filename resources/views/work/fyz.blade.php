27284aafa8c83455788c77cfa018ad5c2eb3107f
MdFaiyyajAlam <mdfaiyyajalam54918@gmail.com>
2020-12-29 19:31:20 +0530

    second work

---
+++ resources/views/work/fyz.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Update Blog Auth</title>
+</head>
+<body>
+    <h2>hi manish kaise ho aap what r u doing men?</h2>
+</body>
+</html>
